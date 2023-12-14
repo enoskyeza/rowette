25823f7eead80604abfc9bd4097100d85d0e0acf
enoskyeza <enoskyeza@gmail.com>
2023-12-14 03:30:39 +0300

    Add animation to logo

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,11 @@ var heroShinker = function() {
         heroHeight = hero.offsetHeight;
     hero.parentNode.style.paddingTop = heroHeight + 'px';
 
+    var logo = document.querySelector('.logo-img'),
+        navLinks = document.querySelector('.nav-links');
+
+
+
     window.addEventListener('scroll', function() {
         var scrollOffset = window.pageYOffset || document.documentElement.scrollTop;
 
@@ -14,6 +19,39 @@ var heroShinker = function() {
             hero.classList.add('fixme');
         } else {
             hero.classList.remove('fixme');
+        }
+
+        if (scrollOffset > 50) {
+            // When scrolled down, shrink the logo and show nav-links
+            logo.style.transform = 'scale(0.3)'; // Adjust the scale factor as needed
+            logo.style.transition = 'transform 0.8s ease';
+            navLinks.style.opacity = '1';
+        } else {
+            // When scrolled back to the top, revert changes
+            logo.style.transform = 'scale(1)';
+            logo.style.transition = 'transform 0.3s ease';
+            navLinks.style.opacity = '0';
+        }
+    });
+};
+
+heroShinker();
+
+var heroShinker = function() {
+
+    window.addEventListener('scroll', function() {
+        var scrollOffset = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollOffset > 50) {
+            // When scrolled down, shrink the logo and show nav-links
+            logo.style.transform = 'scale(0.8)'; // Adjust the scale factor as needed
+            logo.style.transition = 'transform 0.3s ease';
+            navLinks.style.opacity = '1';
+        } else {
+            // When scrolled back to the top, revert changes
+            logo.style.transform = 'scale(1)';
+            logo.style.transition = 'transform 0.3s ease';
+            navLinks.style.opacity = '0';
         }
     });
 };
